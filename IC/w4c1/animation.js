dcb33fc9a34d938a70d9501897ef7f78bc3440c6
JAIMARJOHN <jaykim8313@gmail,com>
2015-09-14 10:14:21 -0700

    inclass ex w4c1

---
+++ IC/w4c1/animation.js
@@ -0,0 +1,22 @@
+
+/**
+ * Created by JAIMARJOHN on 9/14/2015.
+ */
+
+function cl(){
+  alert("IF CLICK WONT DO ANYTHING!");
+
+}
+function msover(){
+  var b =  document.getElementById("1").style.backgroundColor = "black";
+  var b1 = document.getElementById("1").style.color = "white";
+  var b2 = document.getElementById("1").style.opacity = 0.3;
+  return b, b1, b2;
+  }
+
+function mOut() {
+  var b =  document.getElementById("1").style.backgroundColor = "white";
+  var b1 = document.getElementById("1").style.color = "black";
+  var b2 = document.getElementById("1").style.opacity = 1;
+  return b, b1. b2;
+}
