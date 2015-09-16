44a1cc3611123ce61e05228f8ccf2170b2fa9d04
JAIMARJOHN <jaykim8313@gmail,com>
2015-09-16 10:14:05 -0700

    w4c2

---
+++ IC/w4c2/process.js
@@ -0,0 +1,11 @@
+/**
+ * Created by JAIMARJOHN on 9/16/2015.
+ */
+function showInput() {
+  var n =  document.getElementById("username").value;
+  var a = document.getElementById("address").value;
+  var p = document.getElementById("phone").value;
+  document.getElementById('u').innerHTML = n;
+  document.getElementById('a').innerHTML = a;
+  document.getElementById('p').innerHTML = p;
+}
